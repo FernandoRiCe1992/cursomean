a4a502124c71bd113b5da8086c51ac0c47b6c693
Fernando Rizo <fernandorizoceballos@gmail.com>
2026-03-10 23:54:37 -0600

    Se suben controladores y rutas para Album.

--- controllers/album.js
+++ controllers/album.js
@@ -26,6 +26,38 @@ async function getAlbum(req, res){
   }
 };
 
+async function getAlbums(req, res){
+  try{
+    const page = parseInt(req.query.page, 8) || 1;
+    const itemsPerPage = 3;
+
+    const options = {
+      page: page,
+      limit: itemsPerPage,
+      sort: { name: 1 },
+      lean: true
+    };
+    
+    const albums = await Album.paginate({}, options);
+    
+    if(!albums.docs.length){
+      return res.status(404).send({message: 'No hay albums disponibles'});
+    }else{
+      return res.status(200).send({
+        currentPage: albums.page,
+        totalPages: albums.totalPages,
+        docTotal: albums.totalDocs,
+        albums: albums.docs
+
+      })
+    };
+
+  }catch(err){
+    console.log(err);
+    res.status(500).send({message: 'Error del servidor'});
+  };
+};
+
 async function saveAlbum(req, res){
   try{
     const album = new Album();
@@ -52,12 +84,101 @@ async function saveAlbum(req, res){
   };
 };
 
+async function updateAlbum(req, res){
+  try{
+    const albumId = req.query.id;
+    const update = req.body;
+
+    const albumUpdated = await Album.findByIdAndUpdate(albumId, update);
+
+    if(!albumUpdated){
+      res.status(404).send({message: "No se ha actualizado el album"});
+    }else{
+      res.status(200).send({album: albumUpdated});
+    };
+
+  }catch(err){
+    console.log(err);
+    res.status(500).send({message: 'Error del servidor'});
+  };
+};
+
+async function deleteAlbum(req, res){
+  try{
+    const albumId = req.query.id;
+    
+    const albumDeleted = await Album.findByIdAndDelete(albumId);
+
+    if(!albumDeleted){
+      res.status(404).send({message: 'No se ha eliminado el album'});
+    }else{
+      const songDeleted = await Song.find({album: albumDeleted._id}).deleteMany();
+
+      if(!songDeleted){
+        res.status(404).send({message: 'No se ha eliminado las canciones'});
+      }else{
+        res.status(200).send({album: albumDeleted});
+      };
+    };
+
+  }catch(err){
+    console.log(err);
+    res.status(500).send({message: 'Error del servidor'});
+  };
+};
+
+async function uploadImage(req, res){
+  try{
+    const albumId = req.query.id;
+    const file_name = 'No subido...';
+
+    if(req.files){
+      const file_path = req.files.image.path;
+      const file_split = file_path.split('\\');
+      const file_name = file_split[2];
+
+      const ext_split = file_name.split('\.');
+      const file_ext = ext_split[1];
+
+      if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+        const albumUpdated = await Album.findByIdAndUpdate(albumId, {image: file_name});
+
+        if(!albumUpdated){
+          res.status(404).send({message: 'No se actualizo el album'});
+        }else{
+          res.status(200).send({album: albumUpdated});
+        };
+      }else{
+        res.status(400).send({message: 'Extension de imagen invalida'});
+      }
+    }else{
+      res.status(400).send({message: 'No has subido ninguna imagen...'});
+    }
+
+  }catch(err){
+    console.log(err);
+    res.status(500).send({message: 'Error del servidor'});
+  };
+};
+
+async function getImageFile(req, res){
+  const imageFile = req.params.imagefile;
+  const imagePath = './uploads/albums/'+imageFile ;
+
+  if(fs.existsSync(imagePath)){
+    res.sendFile(path.resolve(imagePath));
+  }else{
+    res.status(404).send({message: 'No existe la imagen'});
+  };
+
+};
+
 module.exports = {
   getAlbum,
-  saveAlbum
-  // getAlbums,
-  // updateAlbum,
-  // deleteAlbum,
-  // uploadImage,
-  // getImageFile
+  saveAlbum,
+  getAlbums,
+  updateAlbum,
+  deleteAlbum,
+  uploadImage,
+  getImageFile
 };
--- routes/album.js
+++ routes/album.js
@@ -11,11 +11,11 @@ const md_upload = multiparty({uploadDir: './uploads/albums'});
 const api = express.Router();
 
 api.get('/album', md_auth.ensureAuth, AlbumController.getAlbum);
-//api.get('/albums', md_auth.ensureAuth, AlbumController.getAlbums);
+api.get('/albums', md_auth.ensureAuth, AlbumController.getAlbums);
 api.post('/album', md_auth.ensureAuth, AlbumController.saveAlbum);
-//api.put('/album', md_auth.ensureAuth, AlbumController.updateAlbum);
-//api.delete('/album', md_auth.ensureAuth, AlbumController.deleteAlbum);
-//api.post('/upload-image-album', [md_auth.ensureAuth, md_upload], AlbumController.uploadImage);
-//api.get('/get-image-album/:imagefile', AlbumController.getImageFile);
+api.put('/album', md_auth.ensureAuth, AlbumController.updateAlbum);
+api.delete('/album', md_auth.ensureAuth, AlbumController.deleteAlbum);
+api.post('/upload-image-album', [md_auth.ensureAuth, md_upload], AlbumController.uploadImage);
+api.get('/get-image-album/:imagefile', AlbumController.getImageFile);
 
 module.exports = api;
