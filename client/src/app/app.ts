003b196841a03446488be5c535b81e752a4c70e8
Fernando Rizo <fernandorizoceballos@gmail.com>
2026-03-20 22:35:31 -0600

    Termina la configuracion de modelo y creación del frontend

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { User } from './models/user';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client');
+  // protected readonly title = signal('client');
+  public title = 'MUSICALLY';
+  public user: User;
+  public identity: boolean;
+  public token: any;
+
+  constructor(){
+    this.user = new User('','','','','','ROLE_USER','');
+    this.identity = false;
+  }
 }
